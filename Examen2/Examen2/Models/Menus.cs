e8b0b34139716a4bd43399bd1575255b668c4bac
mariel2494 <dinorammontoya@hotmail.com>
2023-06-12 21:17:47 -0600

    Submit1
    primer submit

---
+++ Examen2/Examen2/Models/Menus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen2.Models
+{
+    public class Menus
+    {
+
+        public string nombre { get; set; }
+        public string urlcategoria { get; set; }
+    }
+}
